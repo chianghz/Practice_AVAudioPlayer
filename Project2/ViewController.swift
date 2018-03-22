52e419e3ba211f74f97f82287fe1467fd2d0413b
Kevin Chiang <kevinchiang.hz@gmail.com>
2018-03-22 23:04:13 +0800

    🔖 Finish

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  let nameOfTheSong = "周杰倫 - 簡單愛 HD"
+
+  var songPlayer = AVAudioPlayer()
+  
+  @IBOutlet weak var lblSpeed: UILabel!
+  @IBOutlet weak var slider: UISlider!
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    prepareSongAndSession()
+  }
+  
+  
+  // MARK: - Actions
+  @IBAction func btn_Pause_Tapped(_ sender: Any) {
+    songPlayer.pause()
+  }
+  @IBAction func btn_Play_Tapped(_ sender: Any) {
+    songPlayer.play()
+  }
+  @IBAction func btn_Stop_Tapped(_ sender: Any) {
+    songPlayer.stop()
+    songPlayer.currentTime = 0
+  }
+  @IBAction func slider_ValueChanged(_ sender: Any) {
+    lblSpeed.text = String(format: "Speed : %.2fx", slider.value)
+    songPlayer.rate = slider.value
+  }
+  
+  
+  // MARK: - Methods
+  func prepareSongAndSession(){
+    
+    do {
+      // Insert the song from our Bundle
+      let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: nameOfTheSong, ofType: "mp3")!)
+      songPlayer = try AVAudioPlayer(contentsOf: url)
+      
+      songPlayer.prepareToPlay()
+      songPlayer.enableRate = true
+      
+      // Create audio session
+      do {
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+      } catch let sessionError {
+        print(sessionError)
+      }
+      
+    } catch let songPlayerError {
+      print(songPlayerError)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  }
+  
+  
 }
+
 
