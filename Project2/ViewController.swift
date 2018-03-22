df884fd513b66cf8877de5a6eb6f5dccaf519af2
Kevin Chiang <kevinchiang.hz@gmail.com>
2018-03-22 15:34:53 +0800

    🎉 Init project

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Kevin Chiang on 2018/3/22.
+//  Copyright © 2018年 KevinChiangHz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
